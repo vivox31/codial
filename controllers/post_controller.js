ac54cc11e9f4fb064506dc54f9f81d7356a9ab2a
vivox31 <vishalkalwani623@gmail.com>
2023-11-06 11:34:20 +0530

    implemented created post and delete post using ajax

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,31 +1,52 @@
 const Post = require('../models/posts');
 const Comment = require('../models/comments')
 
-module.exports.create = function(req,res){
-    const createNewPost = async function(){
-        const newpost = await new Post({content : req.body.content , user:req.user._id});
+module.exports.create = async function (req, res) {
+
+    try {
+        let newpost = await new Post({ content: req.body.content, user: req.user._id });
         newpost.save();
-        
-    }  
-    createNewPost();
-    req.flash('success', 'post created')
-    res.redirect('back')
+
+        if (req.xhr) {
+            return res.status(200).json({
+                data: {
+                    post: newpost
+                },
+                message: "post created!"
+            });
+        }
+        req.flash('success', 'post created')
+        res.redirect('back')
+
+    } catch (err) {
+        colsole.log(err);
+        res.redirect('back')
+    }
 }
 
-module.exports.destroy = async function(req,res){
+module.exports.destroy = async function (req, res) {
 
-   let post = await Post.findById(req.params.id)
-        if(post.user == req.user.id){
+    let post = await Post.findById(req.params.id)
+    if (post.user == req.user.id) {
 
-           await post.deleteOne();
-           await Comment.deleteMany({post:req.params.id})
-           req.flash('success' , 'post deleted')
-            res.redirect('back')
-            
-        }else{
-            req.flash('error', "you can't delete this post!!")
-            res.redirect('back')
+        await post.deleteOne();
+        await Comment.deleteMany({ post: req.params.id })
+
+        if (req.xhr) {
+            return res.status(200).json({
+                data: {
+                    postid: req.params.id
+                },
+                message: "post created!"
+            });
         }
-    
-    
+        req.flash('success', 'post deleted')
+        res.redirect('back')
+
+    } else {
+        req.flash('error', "you can't delete this post!!")
+        res.redirect('back')
+    }
+
+
 }
